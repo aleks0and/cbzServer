2a58f9a8f9793b8ee5b65580cd45be40b54999f1
Aleksander Kusmierczyk <aleksander.kusmierczyk@gmail.com>
2018-12-09 10:13:04 +0200

    updating user location

--- src/main/java/io/swagger/api/UpdateApiController.java
+++ src/main/java/io/swagger/api/UpdateApiController.java
@@ -27,10 +27,9 @@ import java.awt.*;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2018-12-08T19:00:38.207Z[GMT]")
 
 @Controller
@@ -76,10 +75,69 @@ public class UpdateApiController implements UpdateApi {
     }
 
 
-    private boolean checkIfPersonMoved(ArrayList<Object> locationList){
+    private boolean checkIfNotMovedFor10Requestes(ArrayList<Object> locationList){
         List<BigDecimal> listLng = new ArrayList<>();
         List<BigDecimal> listLat = new ArrayList<>();
+        for (Object o : locationList)
+        {
+            String lng = ((Map<String,String>)o).get("lat");
+            String lat = ((Map<String,String>)o).get("lng");
+            listLng.add(new BigDecimal(lng.replaceAll(",", "")));
+            listLat.add(new BigDecimal(lat.replaceAll(",", "")));
+        }
+        for (int i = 1; i < listLat.size(); i++)
+        {
+            double dist = Math.sqrt(Math.pow(listLat.get(i).subtract(listLat.get(i-1)).doubleValue(),2)+Math.pow(listLng.get(i).subtract(listLng.get(i-1)).doubleValue(),2));
+            if (dist > 50)
+                return true;
+        }
+
         return false;
+    }
+    private Map<String,Object> updateLocationRating(Map<String,Object> User, Map<String,Object> location){
+        ArrayList<Object> userDisabilities = (ArrayList<Object>)User.get("disabilityType");
+        Map<String,Object> locationDiabilityRating = new LinkedHashMap<>();
+        if((Map<String, Object>)location.get("placeScore")!= null) {
+            locationDiabilityRating = (Map<String, Object>) location.get("placeScore");
+        }
+        for (Object o: userDisabilities){
+            if (locationDiabilityRating.get((String)o) != null){
+                int currentRating = Integer.parseInt((String)locationDiabilityRating.get((String)o));
+                currentRating ++;
+                locationDiabilityRating.put((String)o,Integer.toString(currentRating));
+            }
+            else{
+                locationDiabilityRating.put((String)o,Integer.toString(1));
+            }
+        }
+        location.put("placeScore",locationDiabilityRating);
+        return location;
+    }
+
+
+    private ArrayList<Double> getDisabilityList(Map<String, Object> entity, boolean isLocation){
+        ArrayList<Double> disabilities = new ArrayList<>();
+        if (entity.size()!=0) {
+            if (isLocation){
+                ArrayList<Object> DisabilityFlags = (ArrayList<Object>)entity.get("placeScore");
+            }
+            else
+            {
+                ArrayList<Object> DisabilityFlags = (ArrayList<Object>)entity.get("disabilityType");
+            }
+        }
+        return disabilities;
+    }
+    private Map<String,Object> findNearestPlace(Map<String,Object> Currentloc)
+    {
+        Map<String,Object> newLocation = new LinkedHashMap<>();
+        newLocation.put("description","sample Disc");
+        newLocation.put("email","email@example.pl");
+        newLocation.put("id","customId");
+        newLocation.put("location",Currentloc);
+        newLocation.put("name","email@example.pl");
+        newLocation.put("phone","customId");
+        return newLocation;
     }
 
     public ResponseEntity<InlineResponse200> updateLocationPost(@ApiParam(value = ""  )  @Valid @RequestBody Object body) {
@@ -115,6 +173,24 @@ public class UpdateApiController implements UpdateApi {
                     databseUpdate.put(k,User.get(k));
                 }
                 databseUpdate.put("lastLocations",locationsUpdate);
+                if (checkIfNotMovedFor10Requestes(locationsUpdate)){
+                    Map<String,Object> loc = new LinkedHashMap<>();
+                    String firebase_baseLocUrl = "https://hackhaton-cbz.firebaseio.com/places/";
+                    firebase_baseLocUrl = firebase_baseUrl + Currentloc.get("lat")+ Currentloc.get("lng");
+                    Firebase firebaseLoc = new Firebase(firebase_baseLocUrl);
+                    Map<String,Object> dataLocBaseBody = (Map<String,Object>)firebaseLoc.get().getBody();
+                    if(dataLocBaseBody.size()!=0)
+                    {
+                        loc = updateLocationRating(User,dataLocBaseBody);
+                    }
+                    else
+                    {
+                        Map<String,Object> newLocation = findNearestPlace(Currentloc);
+                        loc = updateLocationRating(User,newLocation);
+                    }
+                    firebaseLoc.put(loc);
+
+                }
                 firebase.put(databseUpdate);
             }
             else{
